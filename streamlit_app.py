d4e90b380313dc0faead4dde04433d189c6d436b
mikealynch <mikelynch02115@gmail.com>
2024-11-06 12:40:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
+import easyocr
 import streamlit as st
 
 header = st.container()
+data = st.container()
+features = st.container()
+modelTraining = st.container()
 
 with header:
-        st.title('hello world')
+        st.title('lets scan a business card')
+        
+        
+uploaded_file = st.file_uploader("upload a csv file for analysis")
+
+if uploaded_file is not None:
+    st.title('lets scan a business card 2')
+
