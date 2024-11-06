b5428869a9854d57543ba2ae301b45b66615c504
mikealynch <mikelynch02115@gmail.com>
2024-11-06 13:45:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,3 @@ data = st.container()
 features = st.container()
 modelTraining = st.container()
 
-
-
-# Header section for the Streamlit app
-with header:
-    st.title('Let\'s Scan a Business Card')
-
-# File uploader for image input
-uploaded_file = st.file_uploader("Upload a business card image for OCR analysis", type=["jpg", "jpeg", "png", "bmp"])
